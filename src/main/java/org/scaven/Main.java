18106c75fa1e9d9b3b7717d67aa53aa47d565cef
scavenblaze <yashghogare123@gmail.com>
2026-08-03 22:32:27 +0530

    test: working of db table

--- src/main/java/org/scaven/Main.java
+++ src/main/java/org/scaven/Main.java
@@ -1,9 +1,29 @@
 package org.scaven;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.scaven.models.Column;
+import org.scaven.models.Datatype;
+import org.scaven.models.Row;
+import org.scaven.models.Table;
+
+import java.util.List;
+
 public class Main {
     static void main() {
-        //implement the row column table thing
+        Table table = new Table("Student");
+
+        table.addColumn(new Column("id", Datatype.INT));
+        table.addColumn(new Column("name", Datatype.STRING));
+        table.addColumn(new Column("age", Datatype.INT));
+
+        table.addRow(new Row(List.of(1, "Lando", 29)));
+        table.addRow(new Row(List.of(2, "Oscar", 17)));
+        table.addRow(new Row(List.of(3, "Max", 32)));
+
+        System.out.println("Table name is: " + table.getTableName());
+        System.out.println("Column name is: " + table.getColumns());
+        System.out.println("Row name is: " + table.getRows());
+
     }
 }
+
+//TODO: Create test cases to test models
