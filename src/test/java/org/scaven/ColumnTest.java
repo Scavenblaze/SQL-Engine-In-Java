6ac2a2f159c7104d1180ea163700adc8cec91827
scavenblaze <yashghogare123@gmail.com>
2026-08-04 21:52:22 +0530

    feat: Added Testing for Rows and Columns

---
+++ src/test/java/org/scaven/ColumnTest.java
@@ -0,0 +1,63 @@
+package org.scaven;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.scaven.models.Column;
+import org.scaven.models.Datatype;
+import org.scaven.models.Table;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ColumnTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "id, INT",
+            "name, STRING",
+            "marks, DOUBLE",
+            "pass, BOOLEAN"
+    })
+    void shouldCreateColumn(String columnName, Datatype type) {
+        Column column = new Column(columnName, type);
+
+        assertEquals(columnName, column.getColumnName());
+        assertEquals(type, column.getType());
+
+    }
+
+    @Test
+    void shouldAddColumnToTable() {
+        Table table = new Table("TestTable");
+
+        List<Column> expectedColumns = List.of(
+                new Column("id", Datatype.INT),
+                new Column("name", Datatype.STRING),
+                new Column("marks", Datatype.DOUBLE),
+                new Column("pass", Datatype.BOOLEAN)
+        );
+        for  (Column column : expectedColumns) {
+            table.addColumn(column);
+        }
+
+        List<Column> columns = table.getColumns();
+        assertEquals(4,  columns.size());
+
+        assertEquals("id",  columns.get(0).getColumnName());
+        assertEquals(Datatype.INT, columns.get(0).getType());
+
+        assertEquals("name", columns.get(1).getColumnName());
+        assertEquals(Datatype.STRING, columns.get(1).getType());
+
+        assertEquals("marks", columns.get(2).getColumnName());
+        assertEquals(Datatype.DOUBLE, columns.get(2).getType());
+
+        assertEquals("pass", columns.get(3).getColumnName());
+        assertEquals(Datatype.BOOLEAN, columns.get(3).getType());
+    }
+}
+
+//TODO: remove the assertEquals repetition by putting it in a loop
+//TODO: Add more test cases like no columns, one column, etc
---
+++ src/test/java/org/scaven/RowTest.java
@@ -0,0 +1,42 @@
+package org.scaven;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.scaven.models.Row;
+import org.scaven.models.Table;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RowTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, Ezio, 27.5, true",
+            "2, Altair, 21.2, true",
+            "3, Desmond, 9.8, false"
+    })
+    void shouldAddRow(int id, String name, Double marks, Boolean pass) {
+        Row row =  new Row(List.of(id,  name, marks, pass));
+        assertEquals(List.of(id, name, marks, pass), row.getValues());
+    }
+
+    @Test
+    void addRowToTable(){
+        List<Row> expectedRows = List.of(
+                new Row(List.of(1, "Ezio", 27.5, true)),
+                new Row(List.of(2, "Altair", 21.2, true)),
+                new Row(List.of(3, "Desmond", 9.8, false))
+        );
+
+        Table table = new Table("TestTable");
+        for(Row row: expectedRows){
+            table.addRow(row);
+        }
+    }
+}
+
+//TODO: Prevent addition of rows without any columns
+//TODO: Add more test cases like no rows, one row, etc
